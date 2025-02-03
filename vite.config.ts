3fc26b95491c2b09fa999b11f8079e572db79c6c
Sergey Lebedev <54789143+ShootingersNew@users.noreply.github.com>
2025-02-03 09:38:48 +0300

    feat: add event bus (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import federation from "@originjs/vite-plugin-federation"
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,11 +14,15 @@ export default defineConfig({
     vueDevTools(),
     federation({
       name: 'meduza-pet',
+      shared: ['vue'],
       remotes: {
         'meduza-pet-header': 'http://localhost:5005/assets/meduzaHeader.js',
       },
     }),
   ],
+  build:{
+    target: 'esnext',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
