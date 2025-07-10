bab03be3735557c58d66342158fd3fb812dd1c0c
ShootingersNew <lebedevseergey@gmail.com>
2025-07-10 16:14:50 +0300

    fix(useCustomRoute): export

--- src/shared/navigation/useCustomRoute.ts
+++ src/shared/navigation/useCustomRoute.ts
@@ -1,7 +1,8 @@
 import { computed, type ComputedRef } from "vue"
 import type { RouteLocationNormalizedLoaded } from "vue-router"
 
-export function useCustomRoute(): { route: ComputedRef<RouteLocationNormalizedLoaded | undefined> } {
+function useCustomRoute(): { route: ComputedRef<RouteLocationNormalizedLoaded | undefined> } {
   const route = computed(() => window.router?.currentRoute.value)
   return { route }
 }
+export default useCustomRoute
